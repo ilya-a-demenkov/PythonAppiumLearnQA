d563d51669d209752bb9eb0351172f7d68fb0f8a
ilya.a.demenkov <ilya.a.demenkov@gmail.com>
2023-01-12 12:49:42 +0300

    lesson 1 test 1

--- main.py
+++ main.py
@@ -1,19 +1,9 @@
 class MainClass:
-    __class_number = 20
-
-    def getClassNumber(self):
-        return __class_number
 
     def getLocalNumber(self):
         return 14
 
-
 class MainClassTest(MainClass):
-
-    def testGetClassNumber(self):
-        if self.getClassNumber > 45:
-            return "Тест пройден: значение больше 45"
-        return "Тест провален: значение не больше 45"
 
     def testGetLocalNumber(self):
         if self.getLocalNumber == 14:
