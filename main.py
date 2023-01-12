cded0ce57f615d88d210297f829fcf1640dddec7
ilya.a.demenkov <ilya.a.demenkov@gmail.com>
2023-01-12 13:02:00 +0300

    lesson 1 test 3

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 class MainClass:
     __class_number = 20
+    __class_string = "Hello, world"
+
+    def getClassString(self):
+        return __class_string
 
     def getClassNumber(self):
         return __class_number
@@ -8,6 +12,11 @@ class MainClass:
         return 14
 
 class MainClassTest(MainClass):
+
+    def testGetClassString(self):
+        if "hello" in self.getClassString or "Hello" in self.getClassString:
+            return "Тест пройден: строка содержит hello или Hello"
+        return "Тест провален: строка не содержит hello или Hello"
 
     def testGetClassNumber(self):
         if self.getClassNumber > 45:
