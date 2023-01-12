aadb983d9bcb0a1fd77f4f5ad9f51ff7383ad959
ilya.a.demenkov <ilya.a.demenkov@gmail.com>
2023-01-12 12:43:41 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,11 @@
+class MainClass:
+
+    def getLocalNumber(self):
+        return 14
+
+class MainClassTest(MainClass):
+
+    def testGetLocalNumber(self):
+        if self.getLocalNumber == 14:
+            return "Тест пройден: значение 14"
+        return "Тест провален: значение не 14"
